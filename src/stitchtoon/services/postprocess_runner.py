5220ef77b0d7a366d72dd0bdd3b5b64f557ee0d8
Beshr Ghalil <beshrghalil@protonmail.com>
2023-04-26 11:06:51 +0300

    changed postprocess to functional approach

--- src/stitchtoon/services/postprocess_runner.py
+++ src/stitchtoon/services/postprocess_runner.py
@@ -4,28 +4,25 @@ import subprocess
 from ..services.global_logger import logFunc
 
 
-class PostProcessRunner:
-    def run(self, command, **kwargs: dict[str:any]):
-        command_args = kwargs.get("postprocess_args", ())
-        console_func = kwargs.get("console_func", print)
-        return self.call_external_func(command, command_args, console_func)
-
-    @logFunc(inclass=True)
-    def call_external_func(self, command, command_args, console_func):
-        proc = subprocess.Popen(
-            [command, *command_args],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            encoding="utf-8",
-            errors="replace",
-            universal_newlines=True,
-            shell=True,
-        )
-        console_func("Post process started!\n")
-        for line in proc.stdout:
-            console_func(line)
-        console_func("\nPost process finished successfully!\n")
+@logFunc
+def postprocess_run(cmd: str, cmd_args: str = (), console=print):
+    proc = subprocess.Popen(
+        f"{cmd.strip()} {cmd_args.strip()}",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding="utf-8",
+        errors="replace",
+        universal_newlines=True,
+        shell=True,
+    )
+    for line in proc.stdout:
+        console(line)
+    if not proc.stdout:
         proc.stdout.close()
-        return_code = proc.wait()
-        if return_code:
-            raise subprocess.CalledProcessError(return_code, command)
+    return_code = proc.wait()
+    if return_code:
+        console(str(subprocess.CalledProcessError(return_code, cmd)), "error")
+    else:
+        console("\n" + "-" * 15 + "\nPost process finished successfully!\n", "success")
+
+    return return_code
