9c1b11af09083cd2f0adab2f082249d7779b92aa
beshr <beshrghalil@protonmail.com>
2023-05-26 12:40:15 +0300

    deleted old packages

--- src/stitchtoon/detectors/__init__.py
+++
@@ -1,5 +0,0 @@
-from .direct_slicing import DirectSlicingDetector
-from .pixel_comparison import PixelComparisonDetector
-from .selector import select_detector
-
-__all__ = [DirectSlicingDetector, PixelComparisonDetector, select_detector]
