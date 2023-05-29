4924bee8e7aa2113c28e12eabb2ab6f67b8eace0
beshr <beshrghalil@protonmail.com>
2023-05-30 01:30:00 +0300

    added image_manipulator class and test

---
+++ src/stitchtoon/core/image_manipulator.py
@@ -0,0 +1,94 @@
+from ..decorators import validate_format
+from PIL.Image import Image
+import PIL.Image
+from io import BytesIO
+
+
+class ImageManipulator:
+    @staticmethod
+    @validate_format
+    def convert_format(*, image: Image, format: str) -> Image:
+        """in memory convert image format.
+
+        Args:
+            image (Image): image to be converted
+            format (str): format to convert image format to
+
+        Returns:
+            Image: New image specified format
+
+        Raises:
+            UnSupportedFormatError: if format is not supported. see stitchtoon.const.FORMATS.
+        """
+        membuf = BytesIO()
+        image.save(membuf, format=format)
+        converted_image = PIL.Image.open(membuf)
+        return converted_image
+
+    @staticmethod
+    def convert_mode(
+        image: list[Image],
+        mode: str,
+        fill_color: tuple[int, int, int] = (255, 255, 255),
+    ) -> Image:
+        """convert image mode.
+
+        Args:
+            image (Image): image to convert
+            mode (str): (RGB, RGBA),
+            fill_color (tuple[int, int, int]): color to fill transparent areas. Defaults to (255,255,255).
+
+        Returns:
+            Image: new image with mode (mode).
+        """
+        cnvrtd_img = PIL.Image.new(mode, image.size, fill_color)
+        cnvrtd_img.filename = image.filename
+        mask = None
+        if mode.upper() == "RGB":
+            # TODOO: replace with getchannel
+            channels = image.split()
+            mask = channels[3] if len(channels) > 3 else mask
+        cnvrtd_img.paste(image, mask=mask)
+        return cnvrtd_img
+
+    @classmethod
+    def resize_all_width(cls, images: Image, value=None):
+        if value is None:
+            return cls._resize_all_width_auto(images)
+        else:
+            return cls._resize_all_width_fixed(images, value)
+
+    @classmethod
+    def _resize_all_width_fixed(cls, images: Image, value: int) -> list[Image]:
+        resized_imgs = []
+        for image in images:
+            resized_img = cls.resize(image, width=value)
+            resized_imgs.append(resized_img)
+
+        return resized_imgs
+
+    @classmethod
+    def _resize_all_width_auto(cls, images: Image) -> list[Image]:
+        pass
+
+    @classmethod
+    def resize(cls, img, width=None, height=None, respect_ratio=True):
+        if width is None and height is None:
+            raise RuntimeError("at least one of (width, height) should not be None")
+        if (width is None or height is None) and not respect_ratio:
+            raise RuntimeError(
+                "both width and height should not be provided when respect_ratio is False."
+            )
+
+        img_ratio = float(img.height / img.width)
+        if width is None:
+            if img.height == height:
+                return img
+            width = int(img_ratio * height)
+        elif height is None:
+            if img.width == width:
+                return img
+            height = int(img_ratio * width)
+        if height > 0 and width > 0:
+            img = img.resize((width, height), PIL.Image.Resampling.LANCZOS)
+        return img
---
+++ tests/core/test_image_manipulator.py
@@ -0,0 +1,32 @@
+import pytest
+from stitchtoon.core.image_manipulator import ImageManipulator
+from stitchtoon.exc import UnSupportedFormatError
+
+
+class TestImageManipulator:
+    def test_convert_format(self, test_images):
+        convert_to = "tiff"
+        img = test_images[0]
+        converted_image = ImageManipulator.convert_format(image=img, format=convert_to)
+        assert converted_image.format.lower() == convert_to
+
+    def test_convert_unsupported_format(self, test_images):
+        convert_to = "pdf"
+        img = test_images[0]
+
+        with pytest.raises(UnSupportedFormatError):
+            img = ImageManipulator.convert_format(image=img, format=convert_to)
+
+    def test_convert_image_mode(self, test_images_rgb, test_images_rgba):
+        rgb = test_images_rgb[0]
+        rgba = test_images_rgba[0]
+
+        assert ImageManipulator.convert_mode(rgb, "RGBA").mode == "RGBA"
+        assert ImageManipulator.convert_mode(rgba, "RGB").mode == "RGB"
+
+    def test_resize_all_width_fixed_enforce(self, test_images):
+        old_width = [im.width for im in test_images]
+        new_width = old_width[0] // 2
+        new_imgs = ImageManipulator.resize_all_width(test_images, new_width)
+        for img in new_imgs:
+            assert img.width == new_width
