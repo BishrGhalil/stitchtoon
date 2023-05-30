e63fa535570d2541f7657cffe43f8356581b0a81
beshr <beshrghalil@protonmail.com>
2023-05-30 09:56:21 +0300

    updated test from using tmpdir to pytest fixtures

--- tests/core/test_image_io.py
+++ tests/core/test_image_io.py
@@ -1,4 +1,3 @@
-import tempfile
 from pathlib import Path
 
 import pytest
@@ -20,10 +19,9 @@ class TestImageIO:
     def test_load_archive(self, test_archive_file):
         ImageIO.load_archive(path=test_archive_file)
 
-    def test_save_image(self, test_images_rgb):
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            img = test_images_rgb[0]
-            ImageIO.save_image(out=tmpdirname, image=img, format="jpeg", quality=80)
+    def test_save_image(self, test_images_rgb, tmp_path):
+        img = test_images_rgb[0]
+        ImageIO.save_image(out=tmp_path, image=img, format="jpeg", quality=80)
 
     def test_save_images_to_wrong_path(self, test_images_rgb):
         with pytest.raises(FileNotFoundError):
@@ -31,14 +29,13 @@ class TestImageIO:
                 out="jslAhsdFjD98023", image=test_images_rgb, format="jpeg"
             )
 
-    def test_archive_images(self, test_images_rgb):
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            ImageIO.archive_images(
-                out=Path(tmpdirname) / "images.zip",
-                images=test_images_rgb,
-                format="jpeg",
-                quality=80,
-            )
+    def test_archive_images(self, test_images_rgb, tmp_path):
+        ImageIO.archive_images(
+            out=Path(tmp_path) / "images.zip",
+            images=test_images_rgb,
+            format="jpeg",
+            quality=80,
+        )
 
     def test_archive_images_to_wrong_path(self, test_images_rgb):
         with pytest.raises(FileNotFoundError):
@@ -49,21 +46,19 @@ class TestImageIO:
                 quality=80,
             )
 
-    def test_save_all_RGBA_to_RGB_auto_convert(self, test_images_rgba):
-        with tempfile.TemporaryDirectory() as tmpdirname:
+    def test_save_all_RGBA_to_RGB_auto_convert(self, test_images_rgba, tmp_path):
+        ImageIO.save_all(
+            out=tmp_path,
+            images=test_images_rgba,
+            format="jpeg",
+            convert_modes=True,
+        )
+
+    def test_save_all_RGBA_to_RGB_no_convert(self, test_images_rgba, tmp_path):
+        with pytest.raises(OSError):
             ImageIO.save_all(
-                out=tmpdirname,
+                out=tmp_path,
                 images=test_images_rgba,
                 format="jpeg",
-                convert_modes=True,
+                convert_modes=False,
             )
-
-    def test_save_all_RGBA_to_RGB_no_convert(self, test_images_rgba):
-        with pytest.raises(OSError):
-            with tempfile.TemporaryDirectory() as tmpdirname:
-                ImageIO.save_all(
-                    out=tmpdirname,
-                    images=test_images_rgba,
-                    format="jpeg",
-                    convert_modes=False,
-                )
